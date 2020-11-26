6d4b7edcb43e578298d990915c0578775e7936c8
simonojok19 <simonojok19@gmail.com>
2020-11-26 22:53:11 +0300

    made use of IEnumerable<T> and Enumerable<T>

--- Features/Program.cs
+++ Features/Program.cs
@@ -7,16 +7,23 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            Employee[] developers = new Employee[]
+            IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee { Id = 1, Name = "Scott" },
                 new Employee { Id = 2, Name = "Chris" }
             };
 
-            List<Employee> sales = new List<Employee>()
+            IEnumerable<Employee> sales = new List<Employee>()
             {
                 new Employee { Id = 3, Name = "Alex" }
             };
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while(enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
         }
     }
 }
