754cac4a88fc5e94d0954ed0d2dd15274bca5598
simonojok19 <simonojok19@gmail.com>
2020-11-26 22:46:45 +0300

    created instance of employee in collection in the program class of features project

--- Features/Program.cs
+++ Features/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Features
 {
@@ -6,7 +7,16 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee[] developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott" },
+                new Employee { Id = 2, Name = "Chris" }
+            };
+
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex" }
+            };
         }
     }
 }
