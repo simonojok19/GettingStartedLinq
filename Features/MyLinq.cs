b3485c82879be54d4ecd5df670f116590da60b9c
simonojok19 <simonojok19@gmail.com>
2020-11-26 23:17:12 +0300

    created extension method

--- Features/MyLinq.cs
+++ Features/MyLinq.cs
@@ -6,5 +6,14 @@ namespace Features
 {
     public static class MyLinq
     {
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            int count = 0;
+            foreach(var item in sequence)
+            {
+                count++;
+            }
+            return count;
+        }
     }
 }
