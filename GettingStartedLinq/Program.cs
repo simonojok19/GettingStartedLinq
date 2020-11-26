f140bbb44984672e4dcf9122c97603a3da13ea72
simonojok19 <simonojok19@gmail.com>
2020-11-26 22:02:50 +0300

    started the project

---
+++ GettingStartedLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GettingStartedLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
