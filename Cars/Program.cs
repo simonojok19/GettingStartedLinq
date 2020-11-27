599a5245446a36344b05d181138f0e5bdc56525f
simonojok19 <simonojok19@gmail.com>
2020-11-27 22:58:11 +0300

    added to ToCar function

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -22,6 +22,8 @@ namespace Cars
                 .Select(c => c)
                 .First();
 
+            var top = cars.Any(car => car.Manufacturer == "Ford");
+
             foreach(var car in query.Take(10))
             {
                 Console.WriteLine($"{car.Name}: {car.Combined}");
@@ -30,16 +32,39 @@ namespace Cars
          
         private static List<Car> ProcessFile(string path)
         {
-           // var query = from line in File.ReadAllLines(path).Skip(1)
+            // var query = from line in File.ReadAllLines(path).Skip(1)
             //            where line.Length > 1
             //          select Car.ParseFromCsv(line);
 
             return File.ReadAllLines(path)
                 .Skip(1)
                 .Where(line => line.Length > 1)
-                .Select(Car.ParseFromCsv)
+                .ToCar()
                 .ToList();
 
+        }
+    }
+
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach(var line in source)
+            {
+                var columns = line.Split(",");
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+            
         }
     }
 }
