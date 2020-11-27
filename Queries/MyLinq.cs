bae298273a430d81f188fad4ff6000782ed4d940
simonojok19 <simonojok19@gmail.com>
2020-11-27 08:54:53 +0300

    added my own filter extension method

--- Queries/MyLinq.cs
+++ Queries/MyLinq.cs
@@ -6,9 +6,17 @@ namespace Queries
 {
     public static class MyLinq
     {
-        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source)
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
-
+            var result = new List<T>();
+            foreach(var item in source)
+            {
+                if(predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
         }
     }
 }
