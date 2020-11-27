6b1a2ccf94d6779bef65a6baa01e4aae0dfab423
simonojok19 <simonojok19@gmail.com>
2020-11-27 09:14:33 +0300

    using yield

--- Queries/MyLinq.cs
+++ Queries/MyLinq.cs
@@ -8,15 +8,13 @@ namespace Queries
     {
         public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
-            var result = new List<T>();
             foreach(var item in source)
             {
                 if(predicate(item))
                 {
-                    result.Add(item);
+                    yield return item;
                 }
             }
-            return result;
         }
     }
 }
--- Queries/Program.cs
+++ Queries/Program.cs
@@ -16,12 +16,12 @@ namespace Queries
                 new Movie { Title = "Casablanca", Rating = 8.5f, Year = 1942 }
             };
 
-            var query = movies.Filter(m => m.Year > 2000);
+            var query = movies.Where(m => m.Year > 2000);
 
-            foreach(var movie in query)
-            {
-                Console.WriteLine(movie.Title);
-            }
+            //////foreach(var movie in query)
+            //////{
+            //////    console.writeline(movie.title);
+            //////}
         }
     }
 }
