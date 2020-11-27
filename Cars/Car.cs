5af681fcdff0b039c23b1a6f50a3bb0afc0956f4
simonojok19 <simonojok19@gmail.com>
2020-11-27 20:41:41 +0300

    can now read files

--- Cars/Car.cs
+++ Cars/Car.cs
@@ -20,7 +20,14 @@ namespace Cars
             var columns = line.Split(",");
             return new Car
             {
-
+                Year = int.Parse(columns[0]),
+                Manufacturer = columns[1],
+                Name = columns[2],
+                Displacement = double.Parse(columns[3]),
+                Cylinders = int.Parse(columns[4]),
+                City = int.Parse(columns[5]),
+                Highway = int.Parse(columns[6]),
+                Combined = int.Parse(columns[7])
             };
         }
     }
--- Cars/Program.cs
+++ Cars/Program.cs
@@ -10,13 +10,20 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel.csv");
+
+            foreach(var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
         }
 
         private static List<Car> ProcessFile(string path)
         {
-            File.ReadAllLines(path)
+            return File.ReadAllLines(path)
                 .Skip(1)
                 .Where(line => line.Length > 1)
+                .Select(Car.ParseFromCsv)
+                .ToList();
 
         }
     }
