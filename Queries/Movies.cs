8d6ca92fc9587f732c85dc8a63c79b3c46335978
simonojok19 <simonojok19@gmail.com>
2020-11-27 08:18:52 +0300

    created a new project with a new class

---
+++ Queries/Movies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queries
+{
+    public class Movies
+    {
+        public string Title { get; set; }
+        public float Rating { get; set; }
+        public int Year { get; set; }
+
+    }
+}
